79a93d6ead4e519e45b9fa7424436c41e76452c7
GNovich <44025053+GNovich@users.noreply.github.com>
2020-03-22 12:52:18 +0200

    init push

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ try:
 except ImportError:
     from distutils.core import setup
 
-    config = {
-        'description': 'Tools for processing ddsm',
-        'author': 'Francisco Gimenez',
-        'url': 'https://github.com/fjeg/ddsm_tools',
-        'author_email': 'fgimenez@stanford.edu',
-        'version': '0.1',
-        'packages': ['ddsm_tools'],
-        'name': 'ddsm_tools'
-    }
+config = {
+    'description': 'Tools for processing ddsm',
+    'author': 'Francisco Gimenez',
+    'url': 'https://github.com/fjeg/ddsm_tools',
+    'author_email': 'fgimenez@stanford.edu',
+    'version': '0.1',
+    'packages': ['ddsm_tools'],
+    'name': 'ddsm_tools'
+}
 
 setup(**config)
